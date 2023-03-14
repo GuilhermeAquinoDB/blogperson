fd81cdf2c9202cee6f46bd91ae15bd7689c5ef8d
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2023-03-14 14:11:21 -0300

    projeto criado, configurancao do db

---
+++ src/main/java/com/generation/blogperson/BlogpersonApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/blogperson/BlogpersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
