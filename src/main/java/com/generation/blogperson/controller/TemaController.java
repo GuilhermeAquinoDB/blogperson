3f8597601e5d414809421f975204d72374e5dcde
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2023-03-20 20:45:36 -0300

    criar um tema

--- src/main/java/com/generation/blogperson/controller/TemaController.java
+++ src/main/java/com/generation/blogperson/controller/TemaController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogperson.model.Tema;
 import com.generation.blogperson.repository.TemaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/temas")
@@ -37,5 +41,11 @@ public class TemaController {
 	@GetMapping("/descricao/{descricao}")
 	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao) {
 		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(temaRepository.save(tema));
 	}
 }
